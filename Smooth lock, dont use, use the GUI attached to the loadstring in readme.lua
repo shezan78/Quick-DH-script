78f47faa596cc667f749e6b5416ddeeb37c6317f
shezan <97002070+shezan78@users.noreply.github.com>
2023-01-20 21:01:35 -0500

    Update Smooth lock, dont use, use the GUI attached to the loadstring in readme.lua

--- Smooth lock, dont use, use the GUI attached to the loadstring in readme.lua
+++ Smooth lock, dont use, use the GUI attached to the loadstring in readme.lua
@@ -1,1 +1,9 @@
+--[[
+ .--.                      .-. .-.   .-.              .-.   
+: .--'                    .' `.: :   : :              : :.-.
+`. `. ,-.,-.,-. .--.  .--.`. .': `-. : :   .--.  .--. : `'.'
+ _`, :: ,. ,. :' .; :' .; :: : : .. :: :_ ' .; :'  ..': . `.
+`.__.':_;:_;:_;`.__.'`.__.':_; :_;:_;`.__;`.__.'`.__.':_;:_;
+                                                                                                                  
+]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/SmoothLock.lua", true))()
