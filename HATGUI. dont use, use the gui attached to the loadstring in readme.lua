57df42279c96d83c64aea2ff5ef65069d154f722
shezan <97002070+shezan78@users.noreply.github.com>
2023-01-20 21:05:10 -0500

    Update HATGUI. dont use, use the gui attached to the loadstring in readme.lua

--- HATGUI. dont use, use the gui attached to the loadstring in readme.lua
+++ HATGUI. dont use, use the gui attached to the loadstring in readme.lua
@@ -1,1 +1,9 @@
+--[[
+.-..-. .--. .-----.   .--. .-..-..-.
+: :; :: .; :`-. .-'  : .--': :: :: :
+:    ::    :  : :    : : _ : :: :: :
+: :: :: :: :  : :    : :; :: :; :: :
+:_;:_;:_;:_;  :_;    `.__.'`.__.':_;
+                                                                   
+]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/Hat%20Gui.lua", true))()
