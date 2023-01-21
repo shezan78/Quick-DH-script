367eef471102315819b566c59d9529c3b0a3aa07
shezan <97002070+shezan78@users.noreply.github.com>
2023-01-20 20:58:34 -0500

    Create GUI dont use, use the loadstring attached to readme.lua

---
+++ GUI dont use, use the loadstring attached to readme.lua
@@ -0,0 +1,8 @@
+local Library = loadstring(game:HttpGet("https://github.com/shezan78/Kavo-UI-Lib-Remake/blob/main/Kavo%20UI%20Library%20main%20source.lua"))()
+local Window = Library.CreateLib("TITLE", "DarkTheme")
+local Tab = Window:NewTab("Da Hood")
+local Section = Tab:NewSection("Scripts")
+
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked") -- You can also put a loadstring here
+end)
